5a5efd6e7f103c29d4300ade23c1cf0cda323b25
Krishnendu Mitra <111632830+Krishnendu-Mitra@users.noreply.github.com>
2025-03-17 12:01:13 +0530

    Version 1.0

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const http = require('http');
 const path = require('path');
 const fs = require('fs');
 const querystring = require('querystring');
-const ejs = require('ejs');
+// const ejs = require('ejs');
 const jsonfile = require('jsonfile');
 require('./public/App.test.js');
 require('dotenv').config();
 
-class WEB{
+class CDN{
     constructor(port){
         this.active = true;
         this.port = port;
@@ -21,15 +21,12 @@ const app = express();
 let server = http.createServer(app);
 const PORT = process.env.PORT || 8080;
 const AppName = "Cavernous Hoax Scanner";
-let web = new WEB(PORT);
+let cdn = new CDN(PORT);
 
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+/*app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));*/
 
-app.use('/assets',express.static(path.join(__dirname,'assets')));
-app.use('/config',express.static(path.join(__dirname,'config')));
-app.use('/images',express.static(path.join(__dirname,'images')));
 app.use('/public',express.static(path.join(__dirname,'public')));
 
 
@@ -37,43 +34,29 @@ app.use((req, res, next) => {
     try{
         next();
     }catch(e){
-        res.status(401).render('notfound',{error: 401, message: "Unauthorize entry not allow, check the source or report it"});
+        res.status(401).json({error: 401, message: "Unauthorize entry not allow, check the source or report it"});
     }
 });
 
-/*const promises = [
-    ejs.renderFile('./views/header.ejs'),
-    ejs.renderFile('./views/footer.ejs'),
-    ejs.renderFile('./views/service.ejs'),
-    ejs.renderFile('./views/feed.ejs'),
-    ejs.renderFile('./views/faq.ejs')
-];
-
 app.get('/', (req, res) => {
-    Promise.all(promises).then(([header, footer, services, feed, faq]) => {
-        res.status(200).render('index',{header, services, feed, faq, footer});
-    });
+    const redirectUrl = new URL('https://chsweb.vercel.app/cdn');
+    res.status(200).redirect(redirectUrl.href);
 });
 
 app.get('/index', (req, res) => {
     res.redirect('/');
 });
-*/
 
-app.get('/', (req, res) => {
-    res.status(200).render('<h1>Welcome to CHSCDN utility system!</h1><small>This website under development, Please visit our main website for more information</small>');
-});
-
-app.get('/cdn/v1/css/style.css', (req, res) => {
-  res.status(200).sendFile(__dirname + '/cdn/v1/style.min.css');
+app.get('/cdn/v1/css/style.min.css', (req, res) => {
+  res.status(200).sendFile(__dirname + '/public/v1/style.css');
 });
 
 app.get('/cdn/v1/js/script.js', (req, res) => {
-  res.status(200).sendFile(__dirname + '/cdn/v1/script.js');
+  res.status(200).sendFile(__dirname + '/public/v1/script.js');
 });
 
 app.get('*', (req, res) => {
-    res.status(404).render('notfound',{error: 404, message: "Page not found on this url, check the source or report it"});
+    res.status(404).json({error: 404, message: "Page not found on this url, check the source or report it"});
 });
 
 server.listen(PORT, (err) => {
